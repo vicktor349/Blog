e8b375066a07f6d7e72bf80fe6e8dd88001ed72c
vicktoh <ayomidevictor349@gmail.com>
2022-07-04 13:02:19 +0100

    Edited Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 const Navbar = () => {
+    const location = useLocation()
+    const activeColor = (p) => location.pathname === p ? "#f1356d" : "#000"
     return (
+        
         <nav className="navbar">
-            <Link to="/" className="logo-link">Blog</Link>
+            <Link to="/" className="logo-link" style={{color: activeColor("/")}}>Blog</Link>
             <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/create">New Blogs</Link>
+                <Link to="/" style={{color: activeColor("/")}}>Home</Link>
+                <Link to="/create" style={{color: activeColor("/create")}}>New Blogs</Link>
             </div>            
         </nav>
     );
